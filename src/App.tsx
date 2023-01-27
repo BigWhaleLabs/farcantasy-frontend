20bc1c2ef7931924e2fbad3b43b08e5c69425ae3
Nikita Kolmogorov <backmeupplz@gmail.com>
2023-01-27 15:50:26 -0800

    add placeholders

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,74 @@
-import { BodyText } from 'components/Text'
+import { BodyText, TitleText } from 'components/Text'
 import Root from 'components/Root'
+import classnames, {
+  display,
+  flexDirection,
+  gap,
+  listStylePosition,
+  listStyleType,
+  textDecoration,
+} from 'classnames/tailwind'
 
+const textContainer = classnames(
+  display('flex'),
+  flexDirection('flex-col'),
+  gap('gap-2')
+)
+const list = classnames(
+  listStylePosition('list-inside'),
+  listStyleType('list-disc')
+)
+const link = classnames(textDecoration('underline'))
 export default function () {
   return (
     <Root>
-      <BodyText>Hullo world</BodyText>
+      <TitleText>Farcantasy</TitleText>
+      <div className={textContainer}>
+        <BodyText>
+          What the f$%k?! You can now trade Farcaster users? How does this magic
+          work?
+        </BodyText>
+        <BodyText>
+          <ul className={list}>
+            <li>Connect a wallet below to get started</li>
+            <li>Pick a Farcaster user card to mint</li>
+            <li>Mint costs 0.0065 ETH to avoid spam attacks</li>
+            <li>
+              Trade your Farcaster user cards{' '}
+              <a
+                className={link}
+                href="https://opensea.io/assets/ethereum/ethereum/0x2bD8c9F0c485b910C11AB61Fd4145C4FEe31954D"
+              >
+                on OpenSea
+              </a>
+            </li>
+            <li>
+              Each card has attack and defense that depend on number of
+              followers and following
+            </li>
+            <li>
+              You can mint low-stats users to re-sell later if they are
+              promising
+            </li>
+            <li>
+              Initially, only the first 1000 users are available — hurry to get
+              yours until they are all gone!
+            </li>
+            <li>
+              More cards will be released in the future, follow{' '}
+              <a className={link} href="https://fcast.me/borodutch">
+                @borodutch
+              </a>{' '}
+              for updates
+            </li>
+            <li>
+              Will there be a battleground game later with these cards? Who
+              knows!
+            </li>
+          </ul>
+        </BodyText>
+        <BodyText>Now go get 'em, tiger! 🐯</BodyText>
+      </div>
     </Root>
   )
 }
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,26 +1,44 @@
 import {
+  backgroundClip,
+  backgroundImage,
   classnames,
   fontSize,
   fontWeight,
-  textAlign,
+  gradientColorStops,
+  letterSpacing,
+  margin,
   textColor,
+  textTransform,
 } from 'classnames/tailwind'
 import ChildrenProp from 'models/ChildrenProp'
 
 const whiteText = classnames(textColor('text-white'))
-const grayText = classnames(textColor('text-gray-300'))
+
+const titleText = classnames(
+  fontSize('text-5xl', 'md:text-8xl'),
+  fontWeight('font-extrabold'),
+  textColor('text-transparent'),
+  backgroundClip('bg-clip-text'),
+  backgroundImage('bg-gradient-to-r'),
+  gradientColorStops('from-yellow-600', 'to-red-600'),
+  textTransform('uppercase'),
+  letterSpacing('tracking-tighter'),
+  margin('mb-8')
+)
+export function TitleText({ children }: ChildrenProp) {
+  return <h1 className={titleText}>{children}</h1>
+}
 
 const headerText = classnames(
   whiteText,
   fontSize('text-3xl', 'md:text-6xl'),
-  fontWeight('font-bold'),
-  textAlign('text-center')
+  fontWeight('font-bold')
 )
 export function HeaderText({ children }: ChildrenProp) {
   return <p className={headerText}>{children}</p>
 }
 
-const bodyText = classnames(grayText, textAlign('text-center'))
+const bodyText = classnames(textColor('text-purple-200'), fontSize('text-lg'))
 export function BodyText({ children }: ChildrenProp) {
   return <p className={bodyText}>{children}</p>
 }
