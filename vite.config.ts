d947266665ddedc1689aa3b5c231c8476dc84899
Nikita Kolmogorov <backmeupplz@gmail.com>
2023-01-27 18:37:24 -0800

    fix wallets

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,37 @@
-import { defineConfig, Plugin } from 'vite'
+import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { visualizer } from 'rollup-plugin-visualizer'
+import GlobalsPolyfills from '@esbuild-plugins/node-globals-polyfill'
+import nodePolyfills from 'rollup-plugin-polyfill-node'
 
 export default defineConfig({
   plugins: [preact(), tsconfigPaths()],
   build: {
+    outDir: 'docs',
     rollupOptions: {
       plugins: [
         visualizer({
           gzipSize: true,
           brotliSize: true,
-        }) as Plugin,
+        }),
+        nodePolyfills() as any,
       ],
     },
-    outDir: './docs',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        GlobalsPolyfills({
+          buffer: true,
+        }),
+      ],
+    },
+  },
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
   },
 })
