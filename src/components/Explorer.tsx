91922f08b168199b6af3ba19ebbe60fa4cd70b8c
Nikita Kolmogorov <backmeupplz@gmail.com>
2023-01-29 10:14:09 -0800

    start page from 50

--- src/components/Explorer.tsx
+++ src/components/Explorer.tsx
@@ -114,7 +114,7 @@ const buttonRow = classnames(
   padding('px-4', 'py-2')
 )
 export default function () {
-  const [page, setPage] = useState(0)
+  const [page, setPage] = useState(50)
   useEffect(() => {
     state.users = getUsers(page * pageSize, pageSize)
   }, [page])
