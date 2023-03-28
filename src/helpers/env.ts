59246210300f4356ce6a747fddb8809000f4aa7d
Nikita Kolmogorov <backmeupplz@gmail.com>
2023-03-28 13:42:15 -0700

    fix env

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -3,5 +3,5 @@ import { cleanEnv, str } from 'envalid'
 export default cleanEnv(import.meta.env, {
   VITE_ETH_RPC: str(),
   VITE_CONTRACT: str(),
-  VITE_ENVIRONMENT: str(),
+  VITE_ENVIRONMENT: str({ default: 'production' }),
 })
